ae695d28d47a3197e40687a762cd0cb5fbed4265
Esraa Mohamed <esomo60@gmail.com>
2026-09-03 14:24:32 +0300

    PracticalQuestions-Q2-create Shipment struct[fields-properties]

---
+++ Shipment.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AssignmentOOP1
+{
+    internal struct Shipment
+    {
+        #region Fields
+        private string trackingCode;
+        private string description;
+        private decimal weight;
+        private decimal deliveryFee;
+        #endregion
+        #region Properties
+        public string TrackingCode { get { return trackingCode; } }
+        public string Description
+        {
+            get { return description; }
+            set
+            {
+                if (!string.IsNullOrWhiteSpace(value))
+                    description = value;
+            }
+        }
+         public decimal Weight
+        {
+            get { return weight; }
+            set
+            {
+                if (value > 0)
+                weight = value;
+            }
+        }
+        public decimal DeliveryFee
+        {
+            get { return deliveryFee; }
+            private set
+            {
+                if (value > 0)
+                deliveryFee = value;
+            }
+        }
+        public DeliveryAddress Destination { get; set; }
+        public decimal EstimatedCost =>DeliveryFee+(Weight*5);
+        #endregion
+    }
+}
