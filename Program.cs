e057c50a27a98af7d6cbed378d4b93362f5c88bf
Esraa Mohamed <esomo60@gmail.com>
2026-09-03 12:22:42 +0300

    PracticalQuestions-Q1-create DeliveryAddress struct

--- Program.cs
+++ Program.cs
@@ -25,11 +25,25 @@
             //1- no data control and no validation
             //2-no maintainance(modifiaction in more than place)
             //3-no read only /write only(access and print directly)
-            
+
             //b-enhancement with encapsulation
-             //1-put validation on attributes
-             //2- maintain and refactor in one place 
-             //3- read only or write only
+            //1-put validation on attributes
+            //2- maintain and refactor in one place 
+            //3- read only or write only
+            #endregion
+            #region Practical Questions
+            //copying struct
+            //DeliveryAddress originalAddress = new DeliveryAddress("Cairo", "ElTahrir", 5);
+            //DeliveryAddress copiedAddress = originalAddress;
+            //Console.WriteLine("Copying DeliveryAdress..........");
+            //Console.WriteLine("Before Modification");
+            //Console.WriteLine($"originalAddress:{originalAddress.street}");
+            //Console.WriteLine($"copiedAddress:{copiedAddress.street}");
+            //copiedAddress.street = "Orabi";
+            //Console.WriteLine("======================================");
+            //Console.WriteLine("After Modification");
+            //Console.WriteLine($"originalAddress:{originalAddress.street}");//ElTahrir->not change
+            //Console.WriteLine($"copiedAddress:{copiedAddress.street}"); //Orabi
             #endregion
         }
     }
